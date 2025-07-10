59d9c85bc64fa0d63ca1847b3ddecf66fea98278
João Victor Porto <joaovportog@gmail.com>
2025-07-09 22:41:36 -0300

    alterando cruds, e implementando rest freamework

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_create_view, genre_detail_view
+from genres.views import GenreListCreateView, GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_create_view, name='genre-create-list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/', GenreListCreateView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]
